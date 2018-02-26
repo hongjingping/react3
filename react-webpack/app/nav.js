52aab3bd95ad78331f079d029990005c8032a355
wikihong <hjpChinash@163.com>
2018-02-26 14:45:13 +0800

    0226-navlink中的属性

--- react-webpack/app/nav.js
+++ react-webpack/app/nav.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import './nav.css';
+// import './nav.css';
 const NavBar = () => (
   <div>
     <div>
       <NavLink exact className="blue" to="/">wikiHong</NavLink>|&nbsp;
-      <NavLink to="/wikiHong2">wikiHong2</NavLink>|&nbsp;
-      <NavLink to="/wikiHong3">wikiHong3</NavLink>
+      <NavLink style={{color: 'red', fontSize: '20px' }} to="/wikiHong2">wikiHong2</NavLink>|&nbsp;
+      <NavLink activeClassName="pink" to="/wikiHong3">wikiHong3</NavLink>
     </div>
   </div>
 )
